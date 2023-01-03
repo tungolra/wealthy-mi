114a9c1e65d7aafda89fe4011e5aa1dc6d34b62f
rtungol <rarrtungol@gmail.com>
2023-01-03 18:08:16 -0500

    Backend Authentication & Initialize Server
    T-003 - Task - Express app set up, configure database
    T-008 - Task - ctrlr fx registerUser tested; create minimal User model, auth routes set up

---
+++ server/controllers/auth.js
@@ -0,0 +1,42 @@
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcrypt");
+const User = require("../models/user")
+
+async function registerUser(req, res) {
+    const SALT_ROUNDS = await bcrypt.genSalt(10);
+    const hashedPass = await bcrypt.hash(req.body.password, SALT_ROUNDS);
+    req.body.password = hashedPass;
+    const newUser = new User(
+      req.body
+    );
+    const { username } = req.body;
+    try {
+      // check if user already exists
+      const userExists = await User.findOne({ username });
+      if (userExists) {
+        return res
+          .status(400)
+          .json({ message: "Username is already registered" });
+      }
+      //if userdoes not exist, save new user
+      const user = await newUser.save();
+  
+      const token = jwt.sign(
+        {
+          username: user.username,
+          id: user._id,
+        },
+        process.env.JWT_KEY,
+        { expiresIn: "24hr" }
+      );
+      res.status(200).json({ user, token });
+    } catch (error) {
+      res.status(500).json(error.message);
+    }
+}
+async function loginUser(req, res) {}
+
+module.exports = {
+  registerUser,
+  loginUser,
+};
