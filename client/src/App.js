696be44aabc67afd2ad4533d06ffd8a31518ffe6
tungolra <112982647+tungolra@users.noreply.github.com>
2023-01-03 22:37:38 -0500

    Auth (#7)
    * Client Authentication Check
    
    T-007 - Task - set up redux for state management
    
    * Client Authentication UI
    
    T-009 - Task - set up BrowserRouter to render App component in index.js; set up Route to render Auth component in App.js; minimal UI for login/signup
    
    * Backend Authentication & Initialize Server
    
    T-003 - Task - Express app set up, configure database
    T-008 - Task - ctrlr fx registerUser tested; create minimal User model, auth routes set up
    
    * Backend Authentication and Authorization
    
    T-008 - Task - ctrlr fx for loginUser complete
    Note: add proxy to server/package.json and start script as nodemon app.js; removed bin folder and create-express-app defaults
    
    * install packages
    
    * clean up comments
    
    * Client Authentication Check
    
    T-007 - Task - set up redux for state management
    
    * Client Authentication UI
    
    T-009 - Task - set up BrowserRouter to render App component in index.js; set up Route to render Auth component in App.js; minimal UI for login/signup
    
    * install packages
    
    * clean up comments
    
    * Revert "clean up comments"
    
    This reverts commit 182a6464ec8744e8a7d83a39670175f88146e30a.
    
    * Revert "Revert "clean up comments""
    
    This reverts commit 921beafa654e512f54e4c87b8faad3d552ca8cda.

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,16 @@
 import React from "react";
+import Auth from "./pages/Auth/Auth";
+import { Routes, Route, Navigate } from "react-router-dom";
+import { useSelector } from "react-redux";
 
 function App() {
-	return (
-		<div className="App">
-			<p> This is app</p>
-		</div>
-	);
+  return (
+    <div className="App">
+      <Routes>
+        <Route path="/auth" element={<Auth />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
--- client/src/index.js
+++ client/src/index.js
@@ -1,7 +1,9 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import { Provider } from "react-redux";
-import { store } from "./app/store";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+// import { store } from "./app/store";
+import store from "./store/ReduxStore";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 
@@ -9,11 +11,13 @@ const container = document.getElementById("root");
 const root = createRoot(container);
 
 root.render(
-	<React.StrictMode>
-		<Provider store={store}>
-			<App />
-		</Provider>
-	</React.StrictMode>
+  <Provider store={store}>
+    <BrowserRouter>
+      <Routes>
+        <Route path="*" element={<App />} />
+      </Routes>
+    </BrowserRouter>
+  </Provider>
 );
 
 // If you want to start measuring performance in your app, pass a function
