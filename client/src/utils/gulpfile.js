b283ff6a98dc26a73b6aae85c429b9adbcfb1cbd
sk <jsckhan@outlook.com>
2023-01-07 10:37:23 -0500

    style: Update scss for jquery independence
    sb-ui-admin theme has dependencies that do not integrate well with
    React as the theme depends on custom jqeuery that modifies the DOM.
    Custom CSS rules created and added to workflow for integrating sidebar
    elements and future elements.

--- client/src/utils/gulpfile.js
+++ client/src/utils/gulpfile.js
@@ -2,8 +2,70 @@ const gulp = require("gulp");
 const sass = require("gulp-sass")(require("sass"));
 const util = require("gulp-util");
 const del = require("del");
+const merge = require("merge-stream");
 
 const stylesDir = "../styles/";
+const vendorDir = "../styles/vendor/";
+const nodeModDir = "../../node_modules/";
+
+// bring in vendor scss from node packages
+gulp.task("collect_vendor", async () => {
+	function getDependSrc(rel_path) {
+		return nodeModDir + rel_path;
+	}
+
+	function getVendorDir(rel_path) {
+		return vendorDir + rel_path;
+	}
+	// bootstrap JS
+	const bootstrapJS = gulp
+		.src(getDependSrc("bootstrap/dist/js/*"))
+		.pipe(gulp.dest(getVendorDir("bootstrap/js")));
+
+	// bootstrap SCSS
+	const bootstrapSCSS = gulp
+		.src(getDependSrc("bootstrap/scss/**/*.scss"))
+		.pipe(gulp.dest(getVendorDir("bootstrap/scss")));
+
+	// chartJS js
+	const chartJS = gulp
+		.src(getVendorDir("chart.js/dist/*.js"))
+		.pipe(gulp.dest(getVendorDir("chartjs")));
+
+	// dataTables js and css
+	const dataTables = gulp
+		.src([
+			getDependSrc("datatables.net/js/*.js"),
+			getDependSrc("datatables.net-bs4/js/*.js"),
+			getDependSrc("datatables.net-bs4/css/*.css"),
+		])
+		.pipe(gulp.dest(getVendorDir("datatables")));
+
+	// fontwesome
+	const fontAwesome = gulp
+		.src(getDependSrc("@fortawesome/**/*"))
+		.pipe(gulp.dest(getVendorDir(".")));
+
+	// jqeuryEasing js
+	const jqueryEasing = gulp
+		.src(getDependSrc("jquery.easing/*.js"))
+		.pipe(gulp.dest(getVendorDir("jquery-easing")));
+
+	// jquery js
+	const jquery = gulp
+		.src(getDependSrc("jquery/dist/*"))
+		.pipe(gulp.dest(getVendorDir("jquery")));
+
+	await merge(
+		chartJS,
+		bootstrapJS,
+		bootstrapSCSS,
+		dataTables,
+		fontAwesome,
+		jquery,
+		jqueryEasing
+	);
+});
 
 // scss and css tasks
 gulp.task("sass", () => {
