10bec3fea962aa8854d9cd90287b7e76c0f1727b
tungolra <112982647+tungolra@users.noreply.github.com>
2023-01-14 17:09:13 -0500

    Task(s): CRUD funcitons for Expense Tracker; Client Expense Data View and Forms (#13)
    T-013: create expense and category schema, routes and setup for CRUD functions
    T-017: minimal UI to set up client-side scripting for API calls

--- client/src/features/test/test.jsx
+++ client/src/features/test/test.jsx
@@ -1,16 +1,15 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
 import Auth from "../auth/Auth";
+import Expenses from "../expenses/Expense"
 
 const TestRoutes = () => {
-	return (
-		<Routes>
-			<Route
-				path="auth"
-				element={<Auth />}
-			></Route>
-		</Routes>
-	);
+  return (
+    <Routes>
+      <Route path="auth" element={<Auth />}></Route>
+      <Route path="expenses" element={<Expenses />}></Route>
+    </Routes>
+  );
 };
 
 export default TestRoutes;
---
+++ server/models/expense.js
@@ -0,0 +1,45 @@
+const mongoose = require("mongoose");
+
+const CategorySchema = mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true,
+  },
+});
+
+// format category name: "caTegory" => "Category"
+CategorySchema.pre("save", function (next) {
+  this.name =
+    this.name.trim()[0].toUpperCase() + this.name.slice(1).toLowerCase();
+  next();
+});
+
+const ExpenseSchema = mongoose.Schema(
+  {
+    vendor: {
+      type: String,
+      required: true,
+    },
+    category: { type: CategorySchema },
+    posted: { type: Date, default: Date.now },
+    value: {
+      type: Number,
+      min: 0,
+      required: true,
+    },
+    
+  },
+  {
+    timestamps: true,
+  }
+);
+
+ExpenseSchema.CategorySchema.pre("save", function (next) {
+  this.vendor =
+    this.vendor.trim()[0].toUpperCase() + this.vendor.slice(1).toLowerCase();
+  next();
+});
+
+module.exports = mongoose.model("Expense", ExpenseSchema);
