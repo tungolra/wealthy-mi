43bce4720026fdca0f980d785c0b492292238240
tungolra <112982647+tungolra@users.noreply.github.com>
2023-01-14 17:39:26 -0500

    Dev (#16)
    * Fix: models/expense
    
    * Fix: error handler moved and add main to package.json to run nodemon

--- server/app.js
+++ server/app.js
@@ -1,7 +1,6 @@
 var createError = require("http-errors");
 var express = require("express");
 var path = require("path");
-// var cookieParser = require("cookie-parser");
 var logger = require("morgan");
 var cors = require("cors");
 require("dotenv").config();
@@ -13,13 +12,10 @@ var app = express();
 app.use(cors());
 app.use(logger("dev"));
 app.use(express.json());
-// app.use(express.urlencoded({ extended: false }));
-// app.use(cookieParser());
-// app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/users", require("./routes/users"));
 app.use("/auth", require("./routes/auth"));
-app.use("/expenses", require("./routes/expenses"))
+app.use("/expenses", require("./routes/expenses"));
 
 // error handler
 app.use(function (err, req, res, next) {
@@ -35,20 +31,17 @@ app.use(function (err, req, res, next) {
 // serve client
 app.use(express.static(path.join(__dirname, "../client/build/")));
 
-
-// catch 404 and forward to error handler
-app.use(function (req, res, next) {
-	next(createError(404));
-});
-
 app.listen(port, function () {
-	console.log(`Express app running on port ${port}`);
-  });
+  console.log(`Express app running on port ${port}`);
+});
 
 app.get("/*", function (req, res) {
   res.sendFile("index.html", {
-	root: path.join(__dirname, "../client/build/"),
+    root: path.join(__dirname, "../client/build/"),
   });
 });
 
-// module.exports = app;
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+  next(createError(404));
+});
