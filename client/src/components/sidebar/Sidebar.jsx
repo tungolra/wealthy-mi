603fd248da9c4dce82d0bbe43eca2d37daafd5ad
sk <jsckhan@outlook.com>
2023-01-14 13:11:02 -0500

    task: gitignore for compiled css
    css can be recompiled with the toolchain from sass files. Compiled
    css is then redundant.

--- client/src/components/sidebar/Sidebar.jsx
+++ client/src/components/sidebar/Sidebar.jsx
@@ -6,51 +6,59 @@ import NavItem from "./NavItem";
 import { Link } from "react-router-dom";
 import sideBarContent from "./sideBarContent.json";
 
-export default function Sidebar() {
-	return (
-		<Accordion
-			id="accordionSidebar"
-			as="ul"
-			className="navbar-nav bg-gradient-primary sidebar sidebar-dark"
-		>
-			<BrandHeader key="brandHeader" />
-			<NavDivider />
+export default function Sidebar(props) {
+  return (
+    <Accordion
+      id="accordionSidebar"
+      as="ul"
+      className="navbar-nav bg-gradient-primary sidebar sidebar-dark"
+    >
+      <BrandHeader key="brandHeader" />
+      <NavDivider />
+      <button
+        onClick={() => {
+          console.log("button was clicked");
+          props.conAlert();
+        }}
+      >
+        Con Alert Button
+      </button>
 
-			<li
-				key="Demo"
-				className="nav-item"
-			>
-				<Link
-					className="nav-link"
-					to="/app/demo"
-				>
-					<i className="fas fa-fw fa-tachometer-alt"></i>
-					<span>Link Testing : Demo</span>
-				</Link>
-			</li>
+      <li
+        key="Demo"
+        className="nav-item"
+      >
+        <Link
+          className="nav-link"
+          to="/app/demo"
+        >
+          <i className="fas fa-fw fa-tachometer-alt"></i>
+          <span>Link Testing : Demo</span>
+        </Link>
+      </li>
 
-			<li
-				key="Dashboard"
-				className="nav-item"
-			>
-				<Link
-					className="nav-link"
-					to="dashboard"
-				>
-					<i className="fas fa-fw fa-tachometer-alt"></i>
-					<span>Dashboard</span>
-				</Link>
-			</li>
-			<NavDivider />
+      <li
+        key="Dashboard"
+        className="nav-item"
+      >
+        <Link
+          className="nav-link"
+          to="dashboard"
+        >
+          <i className="fas fa-fw fa-tachometer-alt"></i>
+          <span>Dashboard</span>
+        </Link>
+      </li>
+      <NavDivider />
 
-			<div className="sidebar-heading">Demo Information</div>
+      <div className="sidebar-heading">Demo Information</div>
 
-			{
-				// Array.from(sideBarContent).map()
-				sideBarContent.map((navItem, index) => (
-					<NavItem {...{ ...navItem, eventKey: index }}></NavItem>
-				))
-			}
-		</Accordion>
-	);
+      {
+        // Array.from(sideBarContent).map()
+        sideBarContent.map((navItem, index) => (
+          <NavItem {...{ ...navItem, eventKey: index }}></NavItem>
+        ))
+      }
+    </Accordion>
+  );
 }
