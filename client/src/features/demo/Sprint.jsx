c89efa266491e8ee3ecd62ca64796643a8d691a2
sk <jsckhan@outlook.com>
2023-01-15 23:38:48 -0500

    feat: Demo Landing Page
    ADded demo landing page for project with links to workers and about mes.

---
+++ client/src/features/demo/Sprint.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+
+function SprintList({ sprint, children }) {
+  const features = sprint.features.map((feature) => {
+    var isFinished = feature.finished === "true";
+    return (
+      <div className="d-flex flex-row justify-content-start">
+        <div>
+          {isFinished
+            ? <i className="fa-regular fa-square-check"></i>
+            : <i className="fa-regular fa-square"></i>}
+        </div>
+        <div>&nbsp; &nbsp;{feature.name}</div>
+      </div>
+    );
+  });
+
+  return (
+    <>
+      <div className="d-flex justify-content-center">
+        <hr className="divider text-white w-75" />
+      </div>
+      <div className="text-white px-5">
+        <h4>Sprint {sprint.number}</h4>
+        {features}
+      </div>
+    </>
+  );
+}
+
+export default SprintList;
