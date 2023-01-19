7f05615d0368e39151c3e1f95301452b916e4946
tungolra <112982647+tungolra@users.noreply.github.com>
2023-01-18 23:03:53 -0500

    Dev (#26)
    * Fix: models/expense
    
    * Fix: error handler moved and add main to package.json to run nodemon
    
    * Task: Refactor: Login/Sign Up components
    
    T-019: refactor to have auth calls function in separated logincard and signupcard components
    
    * Task: CRUD functions for Expense Tracker
    
    T-013: adding apiSlice for expense fetch calls, adding mockexpenses.json, updating expense model; baseline for refactoring controller
    
    * Task: Client Expense Data View
    
    T-017: complete client-side scripting to fetch expenses from db in Expense component
    Note: need to map data in return
    
    * Task: Autocomplete Login page
    
    T-018: Add DemoLoginCard component and add to Auth.jsx for visitor's to login without signup
    Note: demo@mail.com, pw: 12345 must be added to prod db
    
    * Task: Client Expense Data View
    
    T-017: Display expense data from store in Expenses component
    Note: Refactor in ExpenseList component
    
    * Task: CRUD functions for Expense Tracker
    
    T-013: add refetch data when expense is added or deleted (note: refactor for autorefresh); client-side scripting to delete expense complete
    Note: creating expenses in progress
    
    * Task: updates for deployment to heroku
    
    * Task: Refactor: autorefresh Expenses when updating/deleting
    
    T-031: Refactor complete
    
    * Task: CRUD functions for Expenses
    
    T-013: Refactor createExpense ctrl fx, new category created if none exist; category model complete; category now references expenses
    
    * Task: CRUD functions for Category
    
    T-030: getCategories ctrl fx complete; redux to fetch data on client-side complete; modularize slicers in services/api to separate requests complete
    
    * Deleted Auth_og.jsx file
    
    * Fix to backendURL in authActions for localhost; switched DBs in config
    
    * File reorg
    
    T-020: remove services folder; move authSlice to features/auth; move api folder to features
    
    * Task: File reorg
    
    T-020: remove authService file and imports/reducers from store.js

--- client/src/app/store.js
+++ client/src/app/store.js
@@ -1,19 +1,16 @@
 import { configureStore } from "@reduxjs/toolkit";
 import authReducer from "../features/auth/authSlice";
-import { authApi } from "../features/auth/authService";
 import { expenseSlice } from "../features/api/expenseSlice";
 import { categorySlice } from "../features/api/categorySlice";
 
 const store = configureStore({
   reducer: {
     auth: authReducer,
-    [authApi.reducerPath]: authApi.reducer,
     [expenseSlice.reducerPath]: expenseSlice.reducer,
     [categorySlice.reducerPath]: categorySlice.reducer,
   },
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware()
-      .concat(authApi.middleware)
       .concat(expenseSlice.middleware)
       .concat(categorySlice.middleware),
 });
