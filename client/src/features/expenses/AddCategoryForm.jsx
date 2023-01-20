aede832dfbccea626076201f470c13bdd1118fbc
sk <jsckhan@outlook.com>
2023-01-19 21:47:24 -0500

    feat: Styling and of category component

--- client/src/features/expenses/AddCategoryForm.jsx
+++ client/src/features/expenses/AddCategoryForm.jsx
@@ -1,19 +1,40 @@
 import { useState } from "react";
+import { Collapse } from "react-bootstrap";
+import PageHeader from "../../components/page-content/PageHeader";
+import PageContent from "../../components/page-content/PageContent";
 
 function AddCategory() {
   const [formData, setFormData] = useState([]);
+  const [openCategory, setOpenCategory] = useState(false);
 
   function handleSubmit() {}
-
+  const addCategoryBtn = (
+    <>
+      <a
+        href="#"
+        class="d-none d-sm-inline-block btn btn-sm btn-primary shadow-sm"
+        onClick={() => setOpenCategory(!openCategory)}
+      >
+        <i class="fa-solid fa-paperclip text-white-50"></i> Add Category
+      </a>
+    </>
+  );
   return (
-    <div className="category-form">
-      <h2>Add a Category</h2>
-      <form onClick={handleSubmit}>
-        <label>Category Name</label>
-        <input type="text" />
-        <button type="submit">Submit</button>
-      </form>
-    </div>
+    <>
+      <PageContent>
+        <PageHeader title="" actions={addCategoryBtn}></PageHeader>
+        <Collapse in={openCategory}>
+          <div className="category-form">
+            <h2>Add a Category</h2>
+            <form onClick={handleSubmit}>
+              <label>Category Name</label>
+              <input type="text" />
+              <button type="submit">Submit</button>
+            </form>
+          </div>
+        </Collapse>
+      </PageContent>
+    </>
   );
 }
 
