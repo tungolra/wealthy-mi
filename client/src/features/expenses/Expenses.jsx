fa88dcd96637e1f21ab55106ef63db08ca6e1dd6
sk <jsckhan@outlook.com>
2023-01-19 21:48:16 -0500

    refactor: Refact expenses component with subcomp

--- client/src/features/expenses/Expenses.jsx
+++ client/src/features/expenses/Expenses.jsx
@@ -10,92 +10,19 @@ import PageHeader from "../../components/page-content/PageHeader";
 
 import ExpenseList from "./ExpenseTable";
 import AddCategory from "./AddCategoryForm";
+import AddExpense from "./AddExpenseForm";
 
 export default function Expenses() {
   const userId = localStorage.getItem("user");
-  // const [filteredExpenses, setFilteredExpenses] = useState([]);
-
-  const {
-    data: categories,
-    isLoading, // optional conditional rendering if data is loading
-    isSuccess, // use for conditional rendering if data retrieved successfully
-    isError, //use for conditional rendering when error occurs
-    error, // use to render error
-  } = useGetCategoriesQuery(userId);
-
-  const initialState = {
-    vendor: "",
-    category: "",
-    posted: "",
-    value: "",
-  };
-  const [formData, setFormData] = useState(initialState);
-  const [createExpense] = useCreateExpenseMutation();
-  function handleChange(e) {
-    setFormData({ ...formData, [e.target.name]: e.target.value });
-  }
-  async function handleSubmit(e) {
-    e.preventDefault();
-    try {
-      await createExpense(formData);
-    } catch (error) {
-      console.log(error);
-    }
-  }
 
   return (
     <>
-      {/* For rendering only */}
-      {
-        /* <ul>
-        <label htmlFor="">Categories</label>
-        {categories.map((c) => (
-          <li>{c}</li>
-        ))}
-      </ul> */
-      }
       <Page showTopbar={false}>
         <PageHeader title="Track My Expenses" actions={[]}></PageHeader>
-        <PageContent></PageContent>
+        <AddCategory />
+        <AddExpense />
         <PageContent>
-          <AddCategory />
           <ExpenseList userId={userId} />
-          {/* refactor to place in CreateExpense component */}
-          <div className="expense-form">
-            <form onSubmit={handleSubmit}>
-              <label>Vendor</label>
-              <input
-                name="vendor"
-                type="text"
-                value={formData.vendor}
-                onChange={handleChange}
-              />
-              <label>Category</label>
-              <input
-                name="category"
-                type="text"
-                value={formData.category}
-                onChange={handleChange}
-              />
-              <label>Transaction Date</label>
-              <input
-                name="posted"
-                type="date"
-                value={formData.posted}
-                onChange={handleChange}
-              />
-              <label>Amount</label>
-              <input
-                name="value"
-                type="number"
-                min="0"
-                step="0.01"
-                value={formData.value}
-                onChange={handleChange}
-              />
-              <button type="submit">Submit</button>
-            </form>
-          </div>
         </PageContent>
       </Page>
     </>
