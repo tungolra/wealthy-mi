183df3c0c41323d5c885342008616a62daf62083
sk <jsckhan@outlook.com>
2023-02-15 23:25:25 -0500

    feat: automatic redirection for authenticatio

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import "./styles/css/styles.css";
 import "@fortawesome/fontawesome-free/css/all.min.css";
 import Sidebar from "./components/sidebar/Sidebar";
 import { Navigate, Route, Routes } from "react-router-dom";
+import { useSelector } from "react-redux";
 import Demo from "./features/demo/Demo";
 import ConstructionAlert from "./components/ConstructionAlert";
 import Expenses from "./features/expenses/Expenses";
@@ -13,8 +14,8 @@ import Dashboard from "./features/dashboard/Dashboard";
 import Liability from "./features/liability/Liabilities";
 import Account from "./features/my-accounts/Accounts";
 import AssetLiabilityView from "./features/asset-liability/Asset-Liability";
-
 function App() {
+  const currentUser = useSelector((state) => state.auth);
   let showConAlert = () => {};
 
   const getConAlertHandle = (alertHandle) => {
@@ -25,18 +26,25 @@ function App() {
     <div id="wrapper">
       <ConstructionAlert onMount={getConAlertHandle} />
       <Sidebar conAlert={() => showConAlert(true)} />
-      <Routes>
-        <Route path="demo" element={<Demo />}></Route>
-        <Route path="guide" element={<Guide />}></Route>
-        <Route path="account" element={<Account />}></Route>
-        <Route path="expense" element={<Expenses />}></Route>
-        <Route path="income" element={<Income />}></Route>
-        <Route path="goal" element={<Goal />}></Route>
-        <Route path="dashboard" element={<Dashboard />}></Route>
-        <Route path="asset-liability" element={<AssetLiabilityView />}></Route>
-        <Route path="liability" element={<Liability />}></Route>
-        <Route path="*" ErrorHandling></Route>
-      </Routes>
+      {currentUser.userInfo
+        ? (
+          <>
+            <Routes>
+              <Route path="demo" element={<Demo />}></Route>
+              <Route path="guide" element={<Guide />}></Route>
+              <Route path="account" element={<Account />}></Route>
+              <Route path="expense" element={<Expenses />}></Route>
+              <Route path="income" element={<Income />}></Route>
+              <Route path="goal" element={<Goal />}></Route>
+              <Route path="dashboard" element={<Dashboard />}></Route>
+              <Route path="asset-liability" element={<AssetLiabilityView />}>
+              </Route>
+              <Route path="liability" element={<Liability />}></Route>
+              <Route path="*" ErrorHandling></Route>
+            </Routes>
+          </>
+        )
+        : <Navigate to="/app/auth" />}
     </div>
   );
 }
